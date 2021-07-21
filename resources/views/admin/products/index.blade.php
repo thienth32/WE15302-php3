6428b39b67dcdf1f05b5fc0e9cd5020c7e073cff
thienth32 <64114903+thienth32@users.noreply.github.com>
2021-07-21 19:41:13 +0700

    Update index.blade.php

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,68 +1,77 @@
-{{$schoolname}}
-@dump($system_users)
-<fieldset>
-    <legend>Tìm kiếm</legend>
-    <form action="" method="get">
-        <div>
-            <label for="">Tên sản phẩm</label>
-            <input type="text" name="keyword" @isset($searchData['keyword']) value="{{$searchData['keyword']}}" @endisset>
-        </div>
-        <div>
-            <label for="">Danh mục sản phẩm</label>
-            <select name="cate_id" >
-                <option value="">Tất cả</option>
-                @foreach($cates as $c)
-                <option @if(isset($searchData['cate_id']) && $c->id == $searchData['cate_id']) selected @endif value="{{$c->id}}">{{$c->name}}</option>
-                @endforeach
-            </select>
-        </div>
-        <div>
-            <label for="">Sắp xếp theo</label>
-            <select name="order_by" >
-                <option value="0">Mặc định</option>
-                <option @if(isset($searchData['order_by']) &&  $searchData['order_by'] == 1) selected @endif  value="1">Tên alphabet</option>
-                <option @if(isset($searchData['order_by']) &&  $searchData['order_by'] == 2) selected @endif value="2">Tên giảm dần alphabet</option>
-                <option @if(isset($searchData['order_by']) &&  $searchData['order_by'] == 3) selected @endif value="3">Giá tăng dần</option>
-                <option @if(isset($searchData['order_by']) &&  $searchData['order_by'] == 4) selected @endif value="4">Giá giảm dần</option>
-            </select>
-        </div>
-        <div>
-            <button type="submit">Tìm kiếm</button>
-        </div>
-    </form>
-</fieldset>
-<table>
-    <thead>
-        <th>STT</th>
-        <th>Tên SP</th>
-        <th>Ảnh</th>
-        <th>Danh mục</th>
-        <th>Giá</th>
-        <th>Tags</th>
-        <th></th>
-    </thead>
-    <tbody>
-        @foreach($data_product as $p)
-        <tr>
-            <td>{{(($data_product->currentPage()-1)*20) + $loop->iteration}}</td>
-            <td>{{$p->name}}</td>
-            <td><img src="{{asset( 'storage/' . $p->image)}}" width="70" /></td>
-            <td>
-                <a href="{{route('category.detail', ['id' => $p->cate_id])}}">{{$p->category->name}}</a>
-            </td>
-            <td>{{$p->price}}</td>
-            <td>
-                {{-- @isset($p->tags)
-                    @foreach($p->tags as $t)
-                    <span style="border: solid 1px #ccc; margin-right: 5px;">{{$t->name}}</span>
+@extends('admin.layouts.main')
+
+@section('content')
+
+<form action="" method="get">
+    <div class="row">
+        <div class="col-6">
+            <div class="form-group">
+                <label for="">Tên sản phẩm</label>
+                <input class="form-control" type="text" name="keyword" @isset($searchData['keyword']) value="{{$searchData['keyword']}}" @endisset>
+            </div>
+            <div class="form-group">
+                <label for="">Danh mục sản phẩm</label>
+                <select class="form-control" name="cate_id" >
+                    <option value="">Tất cả</option>
+                    @foreach($cates as $c)
+                    <option @if(isset($searchData['cate_id']) && $c->id == $searchData['cate_id']) selected @endif value="{{$c->id}}">{{$c->name}}</option>
                     @endforeach
-                @endisset --}}
-            </td>
-            <td></td>
-        </tr>
-        @endforeach
+                </select>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="form-group">
+                <label for="">Sắp xếp theo</label>
+                <select class="form-control" name="order_by" >
+                    <option value="0">Mặc định</option>
+                    <option @if(isset($searchData['order_by']) &&  $searchData['order_by'] == 1) selected @endif  value="1">Tên alphabet</option>
+                    <option @if(isset($searchData['order_by']) &&  $searchData['order_by'] == 2) selected @endif value="2">Tên giảm dần alphabet</option>
+                    <option @if(isset($searchData['order_by']) &&  $searchData['order_by'] == 3) selected @endif value="3">Giá tăng dần</option>
+                    <option @if(isset($searchData['order_by']) &&  $searchData['order_by'] == 4) selected @endif value="4">Giá giảm dần</option>
+                </select>
+            </div>
+            <div class="text-center">
+                <br>
+                <button type="submit" class="btn btn-primary">Tìm kiếm</button>
+            </div>
+        </div>
+    </div>
+</form>
+<div class="row">
+    <table class="table table-striped">
+        <thead>
+            <th>STT</th>
+            <th>Tên SP</th>
+            <th>Ảnh</th>
+            <th>Danh mục</th>
+            <th>Giá</th>
+            <th>Tags</th>
+            <th></th>
+        </thead>
+        <tbody>
+            @foreach($data_product as $p)
+            <tr>
+                <td>{{(($data_product->currentPage()-1)*20) + $loop->iteration}}</td>
+                <td>{{$p->name}}</td>
+                <td><img src="{{asset( 'storage/' . $p->image)}}" width="70" /></td>
+                <td>
+                    <a href="{{route('category.detail', ['id' => $p->cate_id])}}">{{$p->category->name}}</a>
+                </td>
+                <td>{{$p->price}}</td>
+                <td>
+                    {{-- @isset($p->tags)
+                        @foreach($p->tags as $t)
+                        <span style="border: solid 1px #ccc; margin-right: 5px;">{{$t->name}}</span>
+                        @endforeach
+                    @endisset --}}
+                </td>
+                <td></td>
+            </tr>
+            @endforeach
+            
+        </tbody>
         
-    </tbody>
-    
-</table>
-{{$data_product->links()}}
+    </table>
+    {{$data_product->links()}}
+</div>
+@endsection
