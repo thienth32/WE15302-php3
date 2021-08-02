2544ccee8af38142d05c0eb7fb782f5932463e85
thienth32 <64114903+thienth32@users.noreply.github.com>
2021-08-02 19:16:53 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -33,4 +33,40 @@ Route::any('logout', function(){
     return redirect(route('login'));
 })->name('logout');
 
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+Route::get('config/prepare-role-n-permission', function(){
+    Route::create(['name' => 'admin']);
+    Role::create(['name' => 'editor']);
+    Role::create(['name' => 'moderator']);
+
+    Permission::create(['name' => 'add product']);
+    Permission::create(['name' => 'remove product']);
+    
+    $adminRole = Role::find(1);
+    $editorRole = Role::find(2);
+    $modRole = Role::find(3);
+    
+    $addProPer = Permission::find(1);
+    $removeProPer = Permission::find(2);
+
+    $adminRole->givePermissionTo($addProPer);
+    $adminRole->givePermissionTo($removeProPer);
+    
+    $editorRole->givePermissionTo($addProPer);
+    
+    $modRole->givePermissionTo($removeProPer);
+
+    // tài khoản id = 2 là tài khoản quản trị tối cao
+    $adminAcc = User::find(2);
+    $adminAcc->assignRole('admin');
+
+    // tài khoản id = 1 là tài khoản của moderator
+    $modAcc = User::find(1);
+    $modAcc->assignRole('moderator');
+
+    return "done";
+
+});
+
 
